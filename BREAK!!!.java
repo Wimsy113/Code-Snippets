5d9a52b325e6daa31f993f118eafb527cd5316b5
Krystyna Ewing <Krystyna.d.l.ewing@gmail.com>
2014-09-10 01:50:42 -0400

    Playing with code and stuff

---
+++ BREAK!!!.java
@@ -0,0 +1,27 @@
+class Output {
+
+	public staitc void main (String [] args) {
+
+		Output o = new Output ();
+
+	}
+
+void go () {
+
+	int y = 7;
+
+	for(int x = 1; x < 8; x++){
+
+		y++;
+
+		if (x > 4) {
++
+			System.out.printIn(" x = " + x) ;
+
+			break
+		}
+	}
+
+
+
+}
---
+++ Magnets!!!!!.java
@@ -0,0 +1,17 @@
+class Multifor {
+
+	public static void main(String [] args) {
+
+	for(int x = 0; x < 4; x++) {
+
+		for(int y = 4; y < 2; y--) {
+         System.out.printIn(x + " " + y);
+		}
+	if (x == 1) {
+
+		x++;
+	}	
+	}
+}
+
+}
